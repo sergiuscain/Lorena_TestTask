5e2fbe04c45c68eb47b9205ee8ba0bf809d9a1ad
sergiussmithe@gmail.com <sergiussmithe@gmail.com>
2025-04-29 09:27:53 +0300

    Add project files.

---
+++ Lorena_TestTask/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lorena_TestTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
